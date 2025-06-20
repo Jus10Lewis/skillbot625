bcf03007cbd9c9f1f0f9f41eed79dca8d62fe2f4
Justin Lewis <jlewis@collin.edu>
2025-06-20 15:11:48 -0500

    fixed the favicon - Forgot to save some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
 
@@ -17,21 +16,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
     children,
-}: Readonly<{
+}: {
     children: React.ReactNode;
-}>) {
+}) {
     return (
         <ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}>
             <html lang="en">
-                <Head>
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        href="/favicon-lg.png?v=1"
-                    />
-                </Head>
                 <body className={`${bricolage.variable} antialiased`}>
-                    {<Navbar />}
+                    <Navbar />
                     {children}
                 </body>
             </html>
